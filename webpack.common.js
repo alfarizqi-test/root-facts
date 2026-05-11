e505862403e937b1f19bf32c0dd9d5caa955bd57
Architect <alfarizqi.stura@gmail.com>
2026-05-11 16:17:06 +0700

    build fix

--- webpack.common.js
+++ webpack.common.js
@@ -3,55 +3,60 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: {
-    app: path.resolve(__dirname, "src/scripts/index.js"),
-  },
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif|ico|svg)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-    parser: {
-      javascript: {
-        importMeta: true,
-      },
-    },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"),
-      scriptLoading: "module",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: "src/model", to: "model", noErrorOnMissing: true },
-        { from: "src/public", to: "", noErrorOnMissing: true },
-      ],
-    }),
-  ],
-  stats: {
-    warningsFilter: /import\.meta/,
-  },
+	entry: {
+		app: path.resolve(__dirname, "src/scripts/index.js"),
+	},
+	output: {
+		filename: "[name].[contenthash].js",
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "/",
+		clean: true,
+	},
+	resolve: {
+		alias: {
+			"onnxruntime-node$": false,
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(png|jpe?g|gif|ico|svg)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+		parser: {
+			javascript: {
+				importMeta: false,
+			},
+		},
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src/index.html"),
+			scriptLoading: "module",
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: "src/model", to: "model", noErrorOnMissing: true },
+				{ from: "src/public", to: "", noErrorOnMissing: true },
+			],
+		}),
+	],
+	stats: {
+		warningsFilter: /import\.meta/,
+	},
 };
